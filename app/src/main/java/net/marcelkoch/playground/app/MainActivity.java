8d28af574245c8db1b8fffe9951dd068415fb8e6
Marcel Koch <marcel.koch@etecture.de>
2014-08-06 19:30:55 +0200

    Session with Smolli

--- app/src/main/java/net/marcelkoch/playground/app/MainActivity.java
+++ app/src/main/java/net/marcelkoch/playground/app/MainActivity.java
@@ -2,20 +2,22 @@ package net.marcelkoch.playground.app;
 
 import android.app.ListActivity;
 import android.app.LoaderManager;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.SimpleCursorAdapter;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 
 
 public class MainActivity extends ListActivity implements LoaderManager.LoaderCallbacks<Cursor> {
@@ -25,12 +27,13 @@ public class MainActivity extends ListActivity implements LoaderManager.LoaderCa
 
     // These are the Contacts rows that we will retrieve
     static final String[] PROJECTION = new String[] {ContactsContract.Data._ID,
-            ContactsContract.Data.DISPLAY_NAME, ContactsContract.CommonDataKinds.StructuredPostal.FORMATTED_ADDRESS};
+            ContactsContract.Data.DISPLAY_NAME};
 
     // This is the select criteria
     static final String SELECTION = "((" +
             ContactsContract.Data.DISPLAY_NAME + " NOTNULL) AND (" +
-            ContactsContract.Data.DISPLAY_NAME + " != '' ))";
+            ContactsContract.Data.DISPLAY_NAME + " != '' )) AND " +
+            ContactsContract.Data.MIMETYPE + " = ?";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +51,7 @@ public class MainActivity extends ListActivity implements LoaderManager.LoaderCa
         root.addView(progressBar);
 
         // For the cursor adapter, specify which columns go into which views
-        String[] fromColumns = {ContactsContract.Data.DISPLAY_NAME, ContactsContract.CommonDataKinds.StructuredPostal.FORMATTED_ADDRESS};
+        String[] fromColumns = {ContactsContract.Data.DISPLAY_NAME};
         int[] toViews = {android.R.id.text1}; // The TextView in simple_list_item_1
 
         // Create an empty adapter we will use to display the loaded data.
@@ -66,7 +69,7 @@ public class MainActivity extends ListActivity implements LoaderManager.LoaderCa
         // Now create and return a CursorLoader that will take care of
         // creating a Cursor for the data being displayed.
         return new CursorLoader(this, ContactsContract.Data.CONTENT_URI,
-                PROJECTION, SELECTION, null, null);
+                PROJECTION, SELECTION, new String[]{ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE}, null);
     }
 
     // Called when a previously created loader has finished loading
@@ -74,6 +77,8 @@ public class MainActivity extends ListActivity implements LoaderManager.LoaderCa
         // Swap the new cursor in.  (The framework will take care of closing the
         // old cursor once we return.)
         mAdapter.swapCursor(data);
+
+        getNameEmailDetails();
     }
 
     // Called when a previously created loader is reset, making the data unavailable
@@ -84,12 +89,59 @@ public class MainActivity extends ListActivity implements LoaderManager.LoaderCa
         mAdapter.swapCursor(null);
     }
 
+//    public void addWatchJob(double longitude, double latitude, float radius) {
+//        LocationManager mgr =
+//                (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+//
+//        mgr.addProximityAlert(longitude, latitude, radius,100000,);
+//    }
+//
+//    public void sendMessage(String text) {
+//        SmsManager manager = SmsManager.getDefault();
+//    }
+
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         Cursor cursor = (Cursor) l.getItemAtPosition(position);
         String name = cursor.getString(1);
         String address = cursor.getString(2);
         startActivity(new Intent(this, MapActivity.class).putExtra("address", address));
+    }
+
+    public ArrayList<String> getNameEmailDetails() {
+        ArrayList<String> emlRecs = new ArrayList<String>();
+        HashSet<String> emlRecsHS = new HashSet<String>();
+        Context context = this;
+        ContentResolver cr = context.getContentResolver();
+        String[] PROJECTION = new String[] { ContactsContract.RawContacts._ID,
+                ContactsContract.Contacts.DISPLAY_NAME,
+                ContactsContract.Contacts.PHOTO_ID,
+                ContactsContract.CommonDataKinds.Email.DATA,
+                ContactsContract.CommonDataKinds.Photo.CONTACT_ID };
+        String order = "CASE WHEN "
+                + ContactsContract.Contacts.DISPLAY_NAME
+                + " NOT LIKE '%@%' THEN 1 ELSE 2 END, "
+                + ContactsContract.Contacts.DISPLAY_NAME
+                + ", "
+                + ContactsContract.CommonDataKinds.Email.DATA
+                + " COLLATE NOCASE";
+        String filter = ContactsContract.CommonDataKinds.Email.DATA + " NOT LIKE ''";
+        Cursor cur = cr.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, PROJECTION, filter, null, order);
+        if (cur.moveToFirst()) {
+            do {
+                // names comes in hand sometimes
+                String name = cur.getString(1);
+                String emlAddr = cur.getString(3);
+
+                // keep unique only
+                if (emlRecsHS.add(emlAddr.toLowerCase())) {
+                    emlRecs.add(emlAddr);
+                }
+            } while (cur.moveToNext());
+        }
+
+        cur.close();
+        return emlRecs;
     }
 
 }
